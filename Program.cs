3bc9ecf179e3881757375fc5d417bf1e33e01c69
Spec-Chum <specchumster@gmail.com>
2024-01-21 15:53:44 +0000

    Refactor, Span was a bit overkill

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Raylib_cs;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,6 +7,14 @@ namespace GameOfLife;
 
 internal static class Program
 {
+    private class Game
+    {
+        public uint[] currentGrid = new uint[GridSize * GridSize];
+        public uint[] oldGrid = new uint[GridSize * GridSize];
+        public bool isRunning;
+        public int fps;
+    }
+
     private const int Width = 960;
     private const int Height = 960;
     private const int GridSize = 64;
@@ -13,121 +22,123 @@ internal static class Program
     private const string TitleString = "Game of Life - ";
     private const string RunningString = TitleString + "Running";
     private const string EditString = TitleString + "Edit";
+    private const string SpeedString = "Speed: ";
 
-    private static readonly uint _alive = (uint)Raylib.ColorToInt(Color.BLUE);
-    private static readonly uint _dead = (uint)Raylib.ColorToInt(Color.BLACK);
-    private static readonly Color _gridColor = Color.DARKGRAY;
-
-    private static bool _isRunning;
-    private static int _fps = 10;
+    private static readonly uint CellAlive = (uint)Raylib.ColorToInt(Color.BLUE);
+    private static readonly uint CellDead = (uint)Raylib.ColorToInt(Color.BLACK);
+    private static readonly Color GridColor = Color.DARKGRAY;
+    private static readonly Color TextColor = Color.DARKGREEN;
 
     private static void Main()
     {
+        Game game = new Game
+        {
+            isRunning = false,
+            fps = 10
+        };
+
         Raylib.InitWindow(Width, Height, EditString);
-        Raylib.SetTargetFPS(_fps);
+        Raylib.SetTargetFPS(game.fps);
 
-        Span<uint> oldGridSpan = stackalloc uint[GridSize * GridSize];
-        Span<uint> currentGridSpan = stackalloc uint[GridSize * GridSize];
-
-        ResetGrid(currentGridSpan);
+        ResetGrid(game);
 
         while (!Raylib.WindowShouldClose())
         {
-            currentGridSpan.CopyTo(oldGridSpan);
+            CopyCurrentGrid(game);
 
-            CheckKeyboardControls(currentGridSpan);
-
-            if (_isRunning)
-            {
-                Run(oldGridSpan, currentGridSpan);
-            }
-            else
-            {
-                CheckMouseControls(currentGridSpan);
-            }
+            CheckKeyboardControls(game);
+            CheckMouseControls(game);
 
             Raylib.BeginDrawing();
-            Raylib.ClearBackground(Raylib.GetColor(_dead));
-            DrawGrid(currentGridSpan);
-            Raylib.DrawText($"Speed: {_fps}", 0, 0, 24, Color.DARKPURPLE);
+            Raylib.ClearBackground(Raylib.GetColor(CellDead));
+            UpdateGrid(game);
+            DrawGridLines();
+            Raylib.DrawText(SpeedString + game.fps, 0, 0, 24, TextColor);
             Raylib.EndDrawing();
         }
 
         Raylib.CloseWindow();
     }
 
-    static ref uint GetCellRef(Span<uint> grid, int x, int y)
+    static ref uint GetCellRef(uint[] grid, int x, int y)
     {
-        ref uint gridRef = ref MemoryMarshal.GetReference(grid);
+        ref uint gridRef = ref MemoryMarshal.GetArrayDataReference(grid);
         return ref Unsafe.Add(ref gridRef, (uint)((y * GridSize) + x));
     }
 
-    private static uint GetCell(ReadOnlySpan<uint> grid, int x, int y)
+    private static uint GetCellValue(uint[] grid, int x, int y)
     {
-        ref uint gridRef = ref MemoryMarshal.GetReference(grid);
+        ref uint gridRef = ref MemoryMarshal.GetArrayDataReference(grid);
         return Unsafe.Add(ref gridRef, (uint)((y * GridSize) + x));
     }
 
-    private static void CheckMouseControls(Span<uint> currentGridSpan)
+    private static void CheckMouseControls(Game game)
     {
         if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT))
         {
-            var mousePos = Raylib.GetMousePosition() / CellSize;
-            GetCellRef(currentGridSpan, (int)mousePos.X, (int)mousePos.Y) = _alive;
+            Vector2 mousePos = Raylib.GetMousePosition() / CellSize;
+            GetCellRef(game.currentGrid, (int)mousePos.X, (int)mousePos.Y) = CellAlive;
         }
         else if (Raylib.IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_RIGHT))
         {
-            var mousePos = Raylib.GetMousePosition() / CellSize;
-            GetCellRef(currentGridSpan, (int)mousePos.X, (int)mousePos.Y) = _dead;
+            Vector2 mousePos = Raylib.GetMousePosition() / CellSize;
+            GetCellRef(game.currentGrid, (int)mousePos.X, (int)mousePos.Y) = CellDead;
         }
     }
 
-    private static void CheckKeyboardControls(Span<uint> currentGridSpan)
+    private static void CheckKeyboardControls(Game game)
     {
         if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
         {
-            _isRunning = !_isRunning;
-            Raylib.SetWindowTitle($"{(_isRunning ? RunningString : EditString)}");
+            game.isRunning = !game.isRunning;
+            Raylib.SetWindowTitle($"{(game.isRunning ? RunningString : EditString)}");
         }
         else if (Raylib.IsKeyPressed(KeyboardKey.KEY_R))
         {
-            ResetGrid(currentGridSpan);
-            _isRunning = false;
+            ResetGrid(game);
+            game.isRunning = false;
             Raylib.SetWindowTitle(EditString);
         }
         else if (Raylib.IsKeyPressed(KeyboardKey.KEY_KP_ADD))
         {
-            _fps += 5;
-            Raylib.SetTargetFPS(_fps);
+            game.fps += 5;
+            Raylib.SetTargetFPS(game.fps);
         }
         else if (Raylib.IsKeyPressed(KeyboardKey.KEY_KP_SUBTRACT))
         {
-            _fps = Math.Max(_fps - 5, 5);
-            Raylib.SetTargetFPS(_fps);
+            game.fps = Math.Max(game.fps - 5, 5);
+            Raylib.SetTargetFPS(game.fps);
         }
     }
 
-    private static void Run(ReadOnlySpan<uint> oldGrid, Span<uint> currentGrid)
+    private static void UpdateGrid(Game game)
     {
         for (int y = 0; y < GridSize; y++)
         {
             for (int x = 0; x < GridSize; x++)
             {
-                switch (GetNumberOfAliveNeighbours(oldGrid, x, y))
+                ref uint currentCellColor = ref GetCellRef(game.currentGrid, x, y);
+
+                if (game.isRunning)
                 {
-                    case 2 when GetCell(oldGrid, x, y) == _alive:
-                    case 3:
-                        GetCellRef(currentGrid, x, y) = _alive;
-                        break;
-                    default:
-                        GetCellRef(currentGrid, x, y) = _dead;
-                        break;
+                    switch (GetNumberOfAliveNeighbours(game, x, y))
+                    {
+                        case 2 when GetCellValue(game.oldGrid, x, y) == CellAlive:
+                        case 3:
+                            currentCellColor = CellAlive;
+                            break;
+                        default:
+                            currentCellColor = CellDead;
+                            break;
+                    }
                 }
+
+                Raylib.DrawRectangle(x * CellSize, y * CellSize, CellSize, CellSize, Raylib.GetColor(currentCellColor));
             }
         }
     }
 
-    private static int GetNumberOfAliveNeighbours(ReadOnlySpan<uint> oldGrid, int x, int y)
+    private static int GetNumberOfAliveNeighbours(Game game, int x, int y)
     {
         int aliveNeighbours = 0;
         for (int i = -1; i <= 1; i++)
@@ -139,7 +150,7 @@ internal static class Program
                     continue;
                 }
 
-                if (GetCell(oldGrid, (x + i + GridSize) % GridSize, (y + j + GridSize) % GridSize) == _alive)
+                if (GetCellValue(game.oldGrid, (x + i + GridSize) % GridSize, (y + j + GridSize) % GridSize) == CellAlive)
                 {
                     aliveNeighbours++;
                 }
@@ -149,36 +160,24 @@ internal static class Program
         return aliveNeighbours;
     }
 
-    private static void ResetGrid(Span<uint> currentGrid)
+    private static void ResetGrid(Game game)
     {
-        currentGrid.Fill(_dead);
+        Array.Fill(game.currentGrid, CellDead);
+    }
+
+    private static void CopyCurrentGrid(Game game)
+    {
+        Array.Copy(game.currentGrid, game.oldGrid, game.currentGrid.Length);
     }
 
     private static void DrawGridLines()
     {
-        Raylib.DrawRectangleLines(0, 0, Width, Height, _gridColor);
+        Raylib.DrawRectangleLines(0, 0, Width, Height, GridColor);
 
         for (int cell = 0; cell < GridSize; cell++)
         {
-            Raylib.DrawLine(0, cell * CellSize, Width, cell * CellSize, _gridColor);
-            Raylib.DrawLine(cell * CellSize, 0, cell * CellSize, Height, _gridColor);
+            Raylib.DrawLine(0, cell * CellSize, Width, cell * CellSize, GridColor);
+            Raylib.DrawLine(cell * CellSize, 0, cell * CellSize, Height, GridColor);
         }
-    }
-
-    private static void DrawGridCells(ReadOnlySpan<uint> currentGrid)
-    {
-        for (int y = 0; y < GridSize; y++)
-        {
-            for (int x = 0; x < GridSize; x++)
-            {
-                Raylib.DrawRectangle(x * CellSize, y * CellSize, CellSize, CellSize, Raylib.GetColor(GetCell(currentGrid, x, y)));
-            }
-        }
-    }
-
-    private static void DrawGrid(ReadOnlySpan<uint> currentGrid)
-    {
-        DrawGridCells(currentGrid);
-        DrawGridLines();
     }
 }
